3ec9d16cf22520b9fe508809b825061d55cc73ca
HwiLu <2418979865@qq.com>
2019-01-03 23:25:33 +0800

    Update 8.2-函数的定义.py

--- Python-basic/8.2-函数的定义.py
+++ Python-basic/8.2-函数的定义.py
@@ -72,18 +72,4 @@ print(id(c), id(d))
 d = c
 print(id(c), id(d))
 
-## 关键字参数
-
-'''
-概念：允许函数调用时参数的顺序与定义时不一致
-
-'''
-def myPrint(str, age):
-    print(str, age)
-
-#使用关键字参数
-myPrint(age = 18, str = "sunck is a good man")
-
-
-
 
