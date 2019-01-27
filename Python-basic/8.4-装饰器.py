aed18d0e4949e30491fe2643dd7df446c4bf6ede
HwiLu <2418979865@qq.com>
2019-01-27 22:13:00 +0800

    Update 8.4-装饰器.py

--- Python-basic/8.4-装饰器.py
+++ Python-basic/8.4-装饰器.py
@@ -1,10 +1,11 @@
 '''
+用的较少
 概念：是一个闭包，把一个函数当做参数,返回一个替代版的函数，本质上就是一个返回函数的函数
 
 '''
 #简单的装饰器
 def func1():
-    print("sunck is a good man")
+    print("hell world")
 
 def outer(func):
     def inner():
